fb52bcb6e944d9767bf5544cbfbfc06e6fae5523
krisswoj <krisswoj@gmail.com>
2018-08-03 18:15:43 +0200

    first version of jQuery implementation

--- new_version/src/main/resources/static/js/datatable.js
+++ new_version/src/main/resources/static/js/datatable.js
@@ -1,14 +1,36 @@
-$(document).ready( function () {
+$(document).ready(function () {
     var table = $('#ingredientsTable').DataTable({
         "sAjaxSource": "/ingredients-list-all",
         "sAjaxDataProp": "",
         "aoColumns": [
-            { "mData": "id"},
-            { "mData": "name" },
-            { "mData": "description" },
-            { "mData": "amountProtins" },
-            { "mData": "amountCarbs" },
-            { "mData": "amountFats" }
-        ]
+            {"mData": "id"},
+            {"mData": "name"},
+            {"mData": "description"},
+            {"mData": "category"},
+            {"mData": "amountProtins"},
+            {"mData": "amountCarbs"},
+            {"mData": "amountFats"}
+        ],
+        
+        initComplete: function () {
+            this.api().columns().every(function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo($(column.footer()).empty())
+                    .on('change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+
+                        column
+                            .search(val ? '^' + val + '$' : '', true, false)
+                            .draw();
+                    });
+
+                column.data().unique().sort().each(function (d, j) {
+                    select.append('<option value="' + d + '">' + d + '</option>')
+                });
+            });
+        }
     })
 });
